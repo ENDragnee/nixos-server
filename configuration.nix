a48cedfc2796b970f7220db127c3507ea2752fb8
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-02-13 14:24:58 +0300

    chore: update docker image to 1.1.2

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,7 @@
   virtualisation.oci-containers = {
     backend = "docker";
     containers."aastu-app" = {
-      image = "mastwal/aastu-slip-app:1.1.1";
+      image = "mastwal/aastu-slip-app:1.1.2";
       ports = ["3000:3000"];
       # Option A: List variables directly (Not recommended for secrets)
       environment = {
